a56c1ef557c61036d62574c64919c0a0fe1e37a5
jamesjkelly <jkellynjus@hotmail.com>
2017-10-21 18:16:39 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,10 @@
+  console.log('Keys is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Ah6cDUAD7zkrNCexzNlm1Vu8O',
+  consumer_secret: 'keOcaWnTqdNuHFYnJktkZokbKtpCmISw1qe1ANRWstoI9aOyZY',
+  access_token_key: '126693143-onBfzg1mDW9JkE1OH5nwVS0pZxxKYVcCGfE6kXrh',
+  access_token_secret: 'CA57jKfno8l8kVzxYwqCrMWEohhNDS4JO0XENDSKeKp4k',
+}
+
+
--- liri.js
+++ liri.js
@@ -1,18 +1,43 @@
-var dataKeys = require("./.gitignore.js");
-var fs = require('fs'); //file system
+console.log(`
+|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+|thanks for checking out my node liri app      |
+|----------------------------------------------|
+|please npm install the following dependencies:|
+|----------------------------------------------|
+|twitter                                       |
+|-------                                       |
+|spotify                                       |
+|-------                                       |
+|request                                       |
+|-------                                       |
+|ENJOY!                                        |
+|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+`);
+// dependencies
+var dataKeys = require("./keys.js");
+var fs = require('fs'); 
 var twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request')
 
+// write to log
+var writeLog = function(data) {
+  fs.appendFile("log.txt", '\r\n\r\n');
+  fs.appendFile("log.txt", JSON.stringify(data), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("wrote to log!");
+  });
+}
+
 // twitter
-var getTweets = function() {
+var myTweets = function() {
 var client = new twitter(dataKeys.twitterKeys);
 var params = { screen_name: 'jkmacman', count: 10 };
-
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
-
     if (!error) {
-      var data = []; //empty array to hold data
+      var data = []; 
       for (var i = 0; i < tweets.length; i++) {
         data.push({
             'created at: ' : tweets[i].created_at,
@@ -23,4 +48,51 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
       }
   });
 };
-const myTweets=  getTweets();
+
+
+
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case 'my-tweets':
+      myTweets();
+      break;
+    case 'spotify-this-song':
+      getMeSpotify(functionData);
+      break;
+    case 'movie-this':
+      getMeMovie(functionData);
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+    default:
+      console.log('LIRI doesn\'t know that');
+  }
+}
+
+
+
+var run = function(caseData, functionData) {
+  switch (caseData) {
+    case 'my-tweets':
+      myTweets();
+      break;
+    case 'spotify-this-song':
+      getMeSpotify(functionData);
+      break;
+    case 'movie-this':
+      getMeMovie(functionData);
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+    default:
+      console.log('Liri not sure, please try again');
+  }
+}
+
+//run
+var runLiri = function(argOne, argTwo) {
+  run(argOne, argTwo);
+};
+runLiri(process.argv[2], process.argv[3]);
