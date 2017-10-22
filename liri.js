0d939c3fc41747644bcbc6f61451ec37e55f0099
jamesjkelly <jkellynjus@hotmail.com>
2017-10-22 00:17:00 -0400

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,18 +1,29 @@
-console.log(`
+console.log(` 
 |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 |thanks for checking out my node liri app      |
 |----------------------------------------------|
 |please npm install the following dependencies:|
 |----------------------------------------------|
 |twitter                                       |
-|-------                                       |
+|----------------------------------------------|
 |spotify                                       |
-|-------                                       |
+|----------------------------------------------|                    
 |request                                       |
-|-------                                       |
+|----------------------------------------------|    
+|To run Liri please enter one of the following:|
+|++++++++++++++++++++++++++++++++++++++++++++++|
+|node liri.js my-tweets                        |
+|----------------------------------------------|
+|node liri.js spotify-this-song 'song title'   |
+|----------------------------------------------|
+|node liri.js movie-this 'movie title'         |
+|----------------------------------------------|
+|node liri.js do-what-it-says                  |
+===============================================|
 |ENJOY!                                        |
 |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 `);
+
 // dependencies
 var dataKeys = require("./keys.js");
 var fs = require('fs'); 
@@ -45,9 +56,66 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
         });
       }
       console.log(data);
+      writeLog(data);
       }
   });
 };
+
+ 
+var movie = function() { 
+// Grab the movieName which will always be the third node argument.
+var movieName = process.argv[3];
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Title:" + JSON.parse(body).Title);
+    console.log("Release Year: " + JSON.parse(body).Year);
+    console.log("Rated:" + JSON.parse(body).Rated);
+    console.log("Plot:" + JSON.parse(body).Plot);
+    console.log("Genre:" + JSON.parse(body).Genre);
+    console.log("Actors:" + JSON.parse(body).Actors);
+    console.log("Writer:" + JSON.parse(body).Writer);
+    console.log("Language:" + JSON.parse(body).Language);
+    console.log("Country:" + JSON.parse(body).Country);
+    console.log("Awards:" + JSON.parse(body).Awards);
+    console.log("Poster:" + JSON.parse(body).Poster);
+
+
+ // "Released": "03 Dec 1976",
+// "Runtime": "120 min",
+// "Genre": "Drama, Sport",
+// "Director": "John G. Avildsen",
+// "Writer": "Sylvester Stallone",
+// "Actors": "Sylvester Stallone, Talia Shire, Burt Young, Carl Weathers",
+// "Plot": "Rocky Balboa, a small-time boxer, gets a supremely rare chance to fight heavy-weight champion Apollo Creed in a bout in which he strives to go the distance for his self-respect.",
+// "Language": "English",
+// "Country": "USA",
+// "Awards": "Won 3 Oscars. Another 16 wins & 21 nominations.",
+// "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY5MDMzODUyOF5BMl5BanBnXkFtZTcwMTQ3NTMyNA@@._V1_SX300.jpg",
+  }
+});
+}
+
+
+function doWhatItSays(){
+    fs.readFile('random.txt','utf8', function(err,data){
+      if (err) { console.log(err) }
+      else {
+        console.log(data)
+      }
+    })
+}
 
 // 
 var run = function(caseData, functionData) {
@@ -59,7 +127,7 @@ var run = function(caseData, functionData) {
       getMeSpotify(functionData);
       break;
     case 'movie-this':
-      getMeMovie(functionData);
+      movie(functionData);
       break;
     case 'do-what-it-says':
       doWhatItSays();
